10b0688e179325f3d87b90413f332e2dd01636e9
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-07 16:27:22 +0530

    bean lifecycle hooks

--- IntroductionToSpringBoot/src/main/java/com/introductionToSpringBoot/IntroductionToSpringBoot/Apple.java
+++ IntroductionToSpringBoot/src/main/java/com/introductionToSpringBoot/IntroductionToSpringBoot/Apple.java
@@ -1,6 +1,8 @@
 package com.introductionToSpringBoot.IntroductionToSpringBoot;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +12,18 @@ public class Apple {
     {
 
         System.out.println("I am eating the apple");
+    }
+
+    @PostConstruct
+    void callThisBeforeUsed()
+    {
+        System.out.println("creating the apple ");
+    }
+
+    @PreDestroy
+    void callThisBeforeDestroy()
+    {
+        System.out.println("Destory the bean");
     }
 
 }
