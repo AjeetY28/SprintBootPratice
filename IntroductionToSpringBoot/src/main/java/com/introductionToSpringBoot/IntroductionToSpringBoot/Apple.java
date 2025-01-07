d23aecc8fd76fa9af54fe7ed630a2bc1d174cadd
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-07 16:07:10 +0530

    creating bean

---
+++ IntroductionToSpringBoot/src/main/java/com/introductionToSpringBoot/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,15 @@
+package com.introductionToSpringBoot.IntroductionToSpringBoot;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Apple {
+    void eatApple()
+    {
+
+        System.out.println("I am eating the apple");
+    }
+
+}
--- IntroductionToSpringBoot/src/main/java/com/introductionToSpringBoot/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ IntroductionToSpringBoot/src/main/java/com/introductionToSpringBoot/IntroductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,13 +1,31 @@
 package com.introductionToSpringBoot.IntroductionToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	Apple obj1;
+
+	@Autowired
+	Apple obj2;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		obj1.eatApple();
+		obj2.eatApple();
+	}
 }
