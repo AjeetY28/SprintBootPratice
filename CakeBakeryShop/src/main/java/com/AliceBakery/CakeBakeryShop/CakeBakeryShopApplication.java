e935f5b742f5b3a2e2dce354498362a7c05acdfc
AjeetY28 <137534276+AjeetY28@users.noreply.github.com>
2025-01-10 01:34:28 +0530

    Creating homework

---
+++ CakeBakeryShop/src/main/java/com/AliceBakery/CakeBakeryShop/CakeBakeryShopApplication.java
@@ -0,0 +1,13 @@
+package com.AliceBakery.CakeBakeryShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeBakeryShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeBakeryShopApplication.class, args);
+	}
+
+}
---
+++ CakeBakeryShop/src/test/java/com/AliceBakery/CakeBakeryShop/CakeBakeryShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AliceBakery.CakeBakeryShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeBakeryShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
