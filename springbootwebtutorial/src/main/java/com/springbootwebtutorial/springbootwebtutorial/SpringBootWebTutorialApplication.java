628c93815bad94b4567e1a496baf9e896c6cd52a
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-11 04:02:56 +0530

    created project structure

---
+++ springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/SpringBootWebTutorialApplication.java
@@ -0,0 +1,20 @@
+package com.springbootwebtutorial.springbootwebtutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebTutorialApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebTutorialApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("creating only project structure");
+		System.out.println("creating only project structure");
+		System.out.println("creating only project structure");
+	}
+}
