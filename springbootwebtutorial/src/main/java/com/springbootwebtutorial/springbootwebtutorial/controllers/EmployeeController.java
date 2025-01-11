004088b45048facf4cd826d13de807dced069984
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-12 03:10:46 +0530

    getmapping by RequestParam

--- springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.springbootwebtutorial.springbootwebtutorial.controllers;
 import com.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -19,5 +20,12 @@ public class EmployeeController {
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId)
     {
         return new EmployeeDTO(employeeId,"Ajeet","ajeet@gmail.com",25, LocalDate.of(2025,1,12),true);
+    }
+
+    @GetMapping("/employee")
+    public String getAllEmployees(@RequestParam(required = false) Integer age,
+                                  @RequestParam(required = false) String sortBy)
+    {
+        return "Hi age "+age+" "+sortBy;
     }
 }
