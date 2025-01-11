3885dd8f5f38523e811e335e1274349343aed566
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-12 03:23:19 +0530

    RequestMapping and varibale name change

--- springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.springbootwebtutorial.springbootwebtutorial.controllers;
 
 import com.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employee") //it is parent path applicable for all these
 public class EmployeeController {
 
 //    @GetMapping(path = "/getSecretMessage")
@@ -16,14 +14,19 @@ public class EmployeeController {
 //    {
 //        return "This is my Secret Message";
 //    }
-    @GetMapping("/employee/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId)
+//    @GetMapping("/employee/{employeeId}")  //when not use RequestMapping
+     @GetMapping("/{employeeId}") //using RequestMapping
+//    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId)//when the both employeeId variable name same for mapping and here
+    public EmployeeDTO getEmployeeById(@PathVariable(name="employeeId") Long id)//when the both employeeId variable not name same for mapping and here
+
     {
-        return new EmployeeDTO(employeeId,"Ajeet","ajeet@gmail.com",25, LocalDate.of(2025,1,12),true);
+        return new EmployeeDTO(id,"Ajeet","ajeet@gmail.com",25, LocalDate.of(2025,1,12),true);
     }
 
-    @GetMapping("/employee")
-    public String getAllEmployees(@RequestParam(required = false) Integer age,
+//    @GetMapping("/employee")  //When Not using RequestMapping
+    @GetMapping //When Using RequestMapping
+//    public String getAllEmployees(@RequestParam(required = false) Integer age, //if variable name change
+    public String getAllEmployees(@RequestParam(required = false,name="inputAge") Integer age, //inputAge will write on web Url
                                   @RequestParam(required = false) String sortBy)
     {
         return "Hi age "+age+" "+sortBy;
