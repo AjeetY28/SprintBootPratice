6fa813b295e2a03935e8fcf0d119a863b6432eb5
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-21 03:13:40 +0530

    encapsulate the error and modified the code

--- springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
+++ springbootwebtutorial/src/main/java/com/springbootwebtutorial/springbootwebtutorial/advices/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ package com.springbootwebtutorial.springbootwebtutorial.advices;
 import com.springbootwebtutorial.springbootwebtutorial.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,7 +25,32 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleInternalServerError(Exception exception)
+    {
+        ApiError apiError=ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleInputValidationErrors(MethodArgumentNotValidException exception)
+    {
+        List<String> errors=exception
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error->error.getDefaultMessage()).
+                collect(Collectors.toList());
 
+        ApiError apiError=ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("Input validation failed")
+                .subErrors(errors)
+                .build();
+        return new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
+    }
 
 }
