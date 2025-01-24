9caced2ae70b73d2d009239e00beb31a7c5845d3
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-25 01:52:29 +0530

    configration on hibernate

--- jpaTuts/src/main/java/com/jpatutorial/jpaTuts/entities/Product.java
+++ jpaTuts/src/main/java/com/jpatutorial/jpaTuts/entities/Product.java
@@ -1,9 +1,6 @@
 package com.jpatutorial.jpaTuts.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table
 public class Product {
 
     @Id
