a32ed6865679b057457fee50d2e62a0465f1777d
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-25 23:41:54 +0530

    run qurry on mysql and also using data.sql file

---
+++ jpaTuts/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into jpatutorial.product_table (price,quantity,sku,title_x) values
+(12.4,4, 'parle734','Parle Buscuit'),
+(14.4,1, 'pepsi123','Pepsi'),
+(34.4,10, 'coke123','Coca Cola'),
+(16.4,3,'pepsi156','Mazza');
