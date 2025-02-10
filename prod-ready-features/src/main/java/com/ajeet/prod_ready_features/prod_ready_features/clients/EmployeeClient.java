0b81897c9c4078671b2473a4e6257ce7f8053e0f
AjeetY28 <ajeet.bca28@gmail.com>
2025-02-11 03:58:15 +0530

    restClient handling

---
+++ prod-ready-features/src/main/java/com/ajeet/prod_ready_features/prod_ready_features/clients/EmployeeClient.java
@@ -0,0 +1,10 @@
+package com.ajeet.prod_ready_features.prod_ready_features.clients;
+
+import com.ajeet.prod_ready_features.prod_ready_features.dto.EmployeeDTO;
+
+import java.util.List;
+
+public interface EmployeeClient {
+
+    List<EmployeeDTO> getAllEmployees();
+}
---
+++ prod-ready-features/src/main/java/com/ajeet/prod_ready_features/prod_ready_features/dto/EmployeeDTO.java
@@ -0,0 +1,35 @@
+package com.ajeet.prod_ready_features.prod_ready_features.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Integer age;
+
+    private String role;
+
+    private Double salary;
+
+    private LocalDate dateOfJoining;
+
+    private Boolean isActive;
+
+
+
+}
