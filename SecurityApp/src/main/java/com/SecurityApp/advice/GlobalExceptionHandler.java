5e8f947eea8a5737a98391d92bf134798f3879e5
AjeetY28 <ajeet.bca28@gmail.com>
2025-03-07 10:31:41 +0530

    5.8 spring security Exception Handler

--- SecurityApp/src/main/java/com/SecurityApp/advice/GlobalExceptionHandler.java
+++ SecurityApp/src/main/java/com/SecurityApp/advice/GlobalExceptionHandler.java
@@ -2,11 +2,15 @@ package com.SecurityApp.advice;
 
 
 import com.SecurityApp.exceptions.ResourceNotFoundException;
+import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +24,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException exception) {
         ApiError apiError=new ApiError(exception.getMessage(),HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleAuthenticationException(AuthenticationException exception) {
+        ApiError apiError=new ApiError(exception.getMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiError> handleJwtException(JwtException exception) {
+        ApiError apiError=new ApiError(exception.getMessage(),HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(apiError,HttpStatus.UNAUTHORIZED);
     }
 }
